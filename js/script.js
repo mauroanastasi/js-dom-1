6421ed4cc33790900db4748158b42c0bf0f9f398
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-26 09:29:57 +0100

    [UPD] fine esercizio Bonus

--- js/script.js
+++ js/script.js
@@ -8,28 +8,48 @@ OnOffButton.addEventListener(`click`, function(){
         accesa.classList.add(`visibile`);
         spenta.classList.remove(`visibile`);
         spenta.classList.add(`nascosta`);
-    } else {
+        OnOffButton.innerText = "spenta"
+
+    } 
+    else {
         accesa.classList.remove(`visibile`);
         accesa.classList.add(`nascosta`);
         spenta.classList.remove(`nascosta`);
         spenta.classList.add(`visibile`);
+        OnOffButton.innerText = "accesa"
     }
 })
+
+// OnOffButton.addEventListener(`click`, function(){
+//     if(OnOffButton.includes("accesa")){
+//         OnOffButton.innerText = "spenta"
+//     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+//     else{
+//         OnOffButton.innerText = "accesa"
+//     }
+// })
 
 
 
 
 
+
+
+
+
+
+
+
+
+// OnOffButton.addEventListener(`click`, function(){
+//     if(accesa.classList.contains(`nascosta`)){
+//         accesa.className += " visibile";
+//         spenta.className += " nascosta";
+//     } else {
+//         accesa.className += "nascosta";
+//         spenta.className += "visibile";
+//     }
+// })
+
+    
